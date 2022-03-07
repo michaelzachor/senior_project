c626640490ff5deaf6dcd12572c6016a4f014db1
Michael Zachor <mfz226@nyu.edu>
2022-03-07 10:30:49 -0500

    cors

--- server/app.js
+++ server/app.js
@@ -16,6 +16,15 @@ mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopolo
 
 app.use(express.json())
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(cors({
   origin: '*',
   methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
