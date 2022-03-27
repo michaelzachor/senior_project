b3ac06303fcb7fad037efc695c28526b183cd359
Michael Zachor <mfz226@nyu.edu>
2022-03-27 19:19:04 -0400

    prepare for heroku

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const albumRoute = require("./routes/albums")
 const cors = require("cors");
 
 const app = express();
-const port = 4000;
+const port = process.env.port || 4000;
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true}, () => {
     console.log("Connected to MongoDB")
