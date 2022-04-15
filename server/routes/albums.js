f99ba398ca308d94425b48822e0006fcbfd9ef6b
Michael Zachor <mfz226@nyu.edu>
2022-04-15 15:36:41 -0400

    cors

--- server/routes/albums.js
+++ server/routes/albums.js
@@ -7,6 +7,15 @@ const cors = require('cors');
 // router.use(cors());
 router.use(cors({origin: /\.herokuapp\.com$/}));
 
+router.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", 'https://warm-jelly-6d1ccf.netlify.app');
+    res.header(
+      "Access-Control-Allow-Headers",
+      // "Origin, X-Requested-With, Content-Type, Accept"
+      "*"
+    );
+    next();
+});
 // check if working 
 router.get('/', (req, res) => {
     res.send("Hello it's album routes"); //runs if we make a get request
